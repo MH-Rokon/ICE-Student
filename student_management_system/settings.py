0932fdad764f2753874e8d9ec9ba8370562327ba
MH Rokon <rokonrokon74805@gmail.com>
2024-06-03 00:12:37 +0600

    m

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+RECAPTCHA_SITE_KEY = '6LdkIO8pAAAAAKhDbgoC9WigryTtNZS5ks0ME6rH'
+RECAPTCHA_SECRET_KEY = '6LdkIO8pAAAAAPNSpvZNNvH2zpAEe2qhb9Ys2Abi'
 import dj_database_url
 import os
 from pathlib import Path
