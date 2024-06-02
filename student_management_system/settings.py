bebd049dd482656fb0cfa20a040441ac7c438c0a
MH Rokon <rokonrokon74805@gmail.com>
2024-06-02 20:40:50 +0600

    m

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -88,20 +88,25 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.mysql',
+# #         'NAME': 'django',
+# #         'USER': 'rokonrokon74805@gmail.com',
+# #         'PASSWORD': '12345679',
+# #         'HOST': '127.0.0.1',
+# #         'PORT': '3307'
+# #     }
+# # python
+# }
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django',
-#         'USER': 'rokonrokon74805@gmail.com',
-#         'PASSWORD': '12345679',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3307'
-#     }
-# python
+    'default': dj_database_url.config(
+        default='postgres://ice_y9d4_user:5urNepYHRYv7wgsHlUyF4uDViNLdS8BW@dpg-cpe5qudds78s73eqr2jg-a.oregon-postgres.render.com/ice_y9d4',
+    )
 }
 
 
