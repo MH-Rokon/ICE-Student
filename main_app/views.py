d311645cc7790dee134607274011023626ebeab0
MH Rokon <rokonrokon74805@gmail.com>
2024-06-03 00:42:33 +0600

    m

--- main_app/views.py
+++ main_app/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.views.decorators.csrf import csrf_exempt
 
-from .EmailBackend import EmailBackend
 from .models import Attendance, Session, Subject
 
 # Create your views here.
@@ -27,7 +26,7 @@ def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
     else:
-        #Google recaptcha
+        # Verify reCAPTCHA token
         captcha_token = request.POST.get('g-recaptcha-response')
         captcha_url = "https://www.google.com/recaptcha/api/siteverify"
         captcha_key = "6LdkIO8pAAAAAPNSpvZNNvH2zpAEe2qhb9Ys2Abi"
@@ -35,20 +34,19 @@ def doLogin(request, **kwargs):
             'secret': captcha_key,
             'response': captcha_token
         }
-        # Make request
         try:
             captcha_server = requests.post(url=captcha_url, data=data)
             response = json.loads(captcha_server.text)
-            if response['success'] == False:
+            if not response['success']:
                 messages.error(request, 'Invalid Captcha. Try Again')
                 return redirect('/')
-        except:
+        except requests.exceptions.RequestException as e:
             messages.error(request, 'Captcha could not be verified. Try Again')
             return redirect('/')
         
-        #Authenticate
-        user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
-        if user != None:
+        # Authenticate user
+        user = authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user is not None:
             login(request, user)
             if user.user_type == '1':
                 return redirect(reverse("admin_home"))
@@ -61,9 +59,8 @@ def doLogin(request, **kwargs):
             return redirect("/")
 
 
-
 def logout_user(request):
-    if request.user != None:
+    if request.user is not None:
         logout(request)
     return redirect("/")
 
@@ -92,35 +89,35 @@ def get_attendance(request):
 def showFirebaseJS(request):
     data = """
     // Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/7.22.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.22.1/firebase-messaging.js');
+    // Note that you can only use Firebase Messaging here, other Firebase libraries
+    // are not available in the service worker.
+    importScripts('https://www.gstatic.com/firebasejs/7.22.1/firebase-app.js');
+    importScripts('https://www.gstatic.com/firebasejs/7.22.1/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-    apiKey: "AIzaSyBarDWWHTfTMSrtc5Lj3Cdw5dEvjAkFwtM",
-    authDomain: "sms-with-django.firebaseapp.com",
-    databaseURL: "https://sms-with-django.firebaseio.com",
-    projectId: "sms-with-django",
-    storageBucket: "sms-with-django.appspot.com",
-    messagingSenderId: "945324593139",
-    appId: "1:945324593139:web:03fa99a8854bbd38420c86",
-    measurementId: "G-2F2RXTL9GT"
-});
+    // Initialize the Firebase app in the service worker by passing in
+    // your app's Firebase config object.
+    // https://firebase.google.com/docs/web/setup#config-object
+    firebase.initializeApp({
+        apiKey: "AIzaSyBarDWWHTfTMSrtc5Lj3Cdw5dEvjAkFwtM",
+        authDomain: "sms-with-django.firebaseapp.com",
+        databaseURL: "https://sms-with-django.firebaseio.com",
+        projectId: "sms-with-django",
+        storageBucket: "sms-with-django.appspot.com",
+        messagingSenderId: "945324593139",
+        appId: "1:945324593139:web:03fa99a8854bbd38420c86",
+        measurementId: "G-2F2RXTL9GT"
+    });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function (payload) {
-    const notification = JSON.parse(payload);
-    const notificationOption = {
-        body: notification.body,
-        icon: notification.icon
-    }
-    return self.registration.showNotification(payload.notification.title, notificationOption);
-});
+    // Retrieve an instance of Firebase Messaging so that it can handle background
+    // messages.
+    const messaging = firebase.messaging();
+    messaging.setBackgroundMessageHandler(function (payload) {
+        const notification = JSON.parse(payload);
+        const notificationOption = {
+            body: notification.body,
+            icon: notification.icon
+        }
+        return self.registration.showNotification(payload.notification.title, notificationOption);
+    });
     """
     return HttpResponse(data, content_type='application/javascript')
